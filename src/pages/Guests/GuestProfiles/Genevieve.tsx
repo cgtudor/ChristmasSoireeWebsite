c74bcc3dacf87cce742aed90ad571f1872b1566f
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-14 17:03:36 +0000

    Add picture for Genevieve and style a bit

--- src/pages/Guests/GuestProfiles/Genevieve.tsx
+++ src/pages/Guests/GuestProfiles/Genevieve.tsx
@@ -1,14 +1,22 @@
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+
+import GenevievePortrait from './GenevievePortrait.png';
 
 const Genevieve = () => {
 	return (
-		<>
-			<Typography variant="h3">Genevieve Auclair</Typography>
-			<Typography variant="subtitle2">
-				Genevieve Auclair is a distinguished lady from Paris. Old friend of the family, she has always brought a sense of elegance to
-				any room she stepped in, turning the eyes of men and women alike.
-			</Typography>
-		</>
+		<Grid container>
+			<Grid item xs={3}>
+				<img src={GenevievePortrait} alt="Genevieve Auclair" style={{ width: '100%' }} />
+			</Grid>
+			<Grid item xs={8} margin={2}>
+				<Typography variant="h3">Genevieve Auclair</Typography>
+				<Typography variant="subtitle2">
+					Genevieve Auclair is a distinguished lady from Paris. Old friend of the family, she has always brought a sense of elegance to
+					any room she stepped in, turning the eyes of men and women alike.
+				</Typography>
+			</Grid>
+		</Grid>
 	);
 };
 
