c537ceabc004c7cb44e651109bb7e87d3de21678
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-18 14:40:41 +0000

    Center letter with table on landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,8 @@ import LetterImg from '~images/ar.png';
 const Home = () => {
 	return (
 		<div className="container pb-[3.5rem]">
-			<img src={TableImg} alt="table" className="table d-[block] mw-[40vw] min-w-[300px] h-[auto] m-[auto]" />
-			<img src={LetterImg} alt="letter front" className="letter d-[block] mw-[40vw] min-w-[300px] h-[auto] m-[auto]" />
+			<img src={TableImg} alt="table" className="table w-[500px]" />
+			<img src={LetterImg} alt="letter front" className="letter w-[700px]" />
 		</div>
 	);
 };
