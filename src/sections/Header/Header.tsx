e77680cf332b6e1687696ad836e1de609fa03437
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-18 22:08:17 +0000

    Make the header transparent and add a bar under it

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,38 +1,44 @@
 import { Link as RouterLink } from 'react-router-dom';
 
-import GitHubIcon from '@mui/icons-material/GitHub';
-import { AppBar, Box, IconButton, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Link, Toolbar, Typography } from '@mui/material';
 
 import Feather from '~images/Logo.png';
 
 const Header = () => {
 	return (
-		<AppBar position="static" color="primary">
-			<Toolbar>
-				<Link component={RouterLink} to="/ChristmasSoireeWebsite/" color="inherit" underline="none">
-					<Box sx={{ display: 'flex', alignItems: 'center' }}>
-						<img src={Feather} alt="Logo" style={{ width: '100px', marginRight: '0.5rem' }} />
-						<Typography variant="h6" component="div">
-							Christmas Soirée
-						</Typography>
-					</Box>
-				</Link>
-				<Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
-					<Link component={RouterLink} to="/ChristmasSoireeWebsite/" color="inherit" underline="none" sx={{ marginLeft: '1rem' }}>
-						Home
-					</Link>
-					<Link
-						component={RouterLink}
-						to="/ChristmasSoireeWebsite/guests"
-						color="inherit"
-						underline="none"
-						sx={{ marginLeft: '1rem' }}
-					>
-						Guests
-					</Link>
+		//<AppBar position="static" sx={{ backgroundColor: 'transparent' }}>
+		<Toolbar
+			sx={{
+				'position': 'relative',
+				'&::after': {
+					content: '""',
+					position: 'absolute',
+					bottom: 0,
+					left: '12.5%',
+					width: '75%',
+					height: '1px',
+					backgroundColor: 'black',
+				},
+			}}
+		>
+			<Link component={RouterLink} to="/ChristmasSoireeWebsite/" color="inherit" underline="none">
+				<Box sx={{ display: 'flex', alignItems: 'center' }}>
+					<img src={Feather} alt="Logo" style={{ width: '100px', marginRight: '0.5rem' }} />
+					<Typography variant="h6" component="div">
+						Christmas Soirée
+					</Typography>
 				</Box>
-			</Toolbar>
-		</AppBar>
+			</Link>
+			<Box sx={{ display: 'flex', justifyContent: 'center', flexGrow: 1 }}>
+				<Link component={RouterLink} to="/ChristmasSoireeWebsite/" color="inherit" underline="none" sx={{ marginLeft: '1rem' }}>
+					Home
+				</Link>
+				<Link component={RouterLink} to="/ChristmasSoireeWebsite/guests" color="inherit" underline="none" sx={{ marginLeft: '1rem' }}>
+					Guests
+				</Link>
+			</Box>
+		</Toolbar>
+		//</AppBar>
 	);
 };
 
