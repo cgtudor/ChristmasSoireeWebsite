f06d31f4096e93f953da26b595f5dd5158fda87c
Calea Dora <dora.calea@gmail.com>
2023-11-18 15:30:23 +0200

    Add landing page letter and restyling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,13 @@
+import TableImg from '~images/Table.svg';
+import LetterImg from '~images/ar.png';
+
 const Home = () => {
-	return <></>;
+	return (
+		<div className="container pb-[3.5rem]">
+			<img src={TableImg} alt="table" className="table d-[block] mw-[40vw] min-w-[300px] h-[auto] m-[auto]" />
+			<img src={LetterImg} alt="letter front" className="letter d-[block] mw-[40vw] min-w-[300px] h-[auto] m-[auto]" />
+		</div>
+	);
 };
 
 export default Home;
