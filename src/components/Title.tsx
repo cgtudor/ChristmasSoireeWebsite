8d4c546ac89039fd61d9502f55a935d3218bd176
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-19 20:26:08 +0000

    Make title of character pages cursive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,7 @@ interface TitleProps {
 }
 
 const Title = ({ text }: TitleProps) => (
-	<Typography variant="h3" sx={{ fontWeight: 'bold', color: '#f5f5f5', marginBottom: '1rem' }}>
+	<Typography variant="h3" fontFamily="Great Vibes, cursive" sx={{ fontWeight: 'bold', color: '#f5f5f5', marginBottom: '1rem' }}>
 		{text}
 	</Typography>
 );
