a19a8f86d470e5dc3fb4a408204a8366d132afc3
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-20 12:26:48 +0000

    Add information about Dick to guests' secrets

--- src/pages/Secrets/SecretsList.ts
+++ src/pages/Secrets/SecretsList.ts
@@ -5,10 +5,14 @@ export const secrets: Record<string, string> = {
 		"You have been previously hired by the actress Catherine, who happens to be one of the guests of tonight's party, to kill her competition for an important main role. Unfortunately you were a bit careless and she found out your real identity. She is also a terrible liar. If she is about to slip up your identity during the night, you must stop her.",
 	TheUnseenBladeSecondQuest:
 		"Your next target is Catherine. She knows too much, it's too dangerous to let her live after all. You must slip a vial of poison in her drink or food by the end of the night. The vial will be provided by one of the hosts upon arriving at the party.",
+	TheUnseenBladeDickInfo:
+		"You know Dick. He's a run of the mill detective as far as you are concerned. He's solved a couple miracle cases, but anyone can do that. Not a threat.",
 	IfATreeFallsFirstQuest:
 		'Make someone believe that you are not actually russian, but an American spy. You must not use the word spy or any of its synonyms. The hosts will ask the guest you picked at the end of the night if they were fooled.',
 	IfATreeFallsSecondQuest:
 		'Bring up quantum physics in five different conversations before the end of the night. Only conversations that count are the ones that were not originally about physics.',
+	IfATreeFallsDickInfo:
+		'You remember Richard from the last party. He came with a date called Nora; a nurse you met a couple times while performing some experiments at the hopspital she was working at.',
 	DickGumshoeFirstQuest:
 		"You don't remember anything about yourself. Figure out as much as you can from the other guests. You have by the end of the night to collate your findings and paint a picture of who you are.",
 	DickGumshoeSecondQuest:
@@ -17,10 +21,14 @@ export const secrets: Record<string, string> = {
 		'Get the hosts (or preferable other guests) to refill your glass five times. The glass must not be empty when you ask for a refill. You must not ask for a refill more than twice per guest.',
 	TooGlamToGiveADamnSecondQuest:
 		'Make a loud toast to the hosts, all the guests (by name for all of them), your mother, your sister and your dog. You must not mention your father. You must not let anyone interrupt you. The toas has to be at least 30 seconds long.',
+	TooGlamToGiveADamnDickInfo:
+		'You know Richard, of course. You know everyone after all. He is a detective with a drinking problem. You have no idea how he ever solves any cases, but you know last party he brought a date called Nora. You have noticed him getting flirty with other guests, and when you confronted him about it you found out that he was cheating on Nora. Another one for the blackmail pile.',
 	ICanDoAllThisThroughHimWhoGivesMeStrengthFirstQuest:
 		'The Holy Vatican has signalled that one of the guests at this party is possessed by an unholy demon. Figure out which and exorcise them. (One of the hosts must see the exorcism happen)',
 	ICanDoAllThisThroughHimWhoGivesMeStrengthSecondQuest:
 		'At any point of the party, stop everyone to hold a quick prayer. You must be the one reciting the prayer, and if anyone is not praying with their eyes closed you must admonish them.',
+	ICanDoAllThisThroughHimWhoGivesMeStrengthDickInfo:
+		'You know Richard. Detective. Pretty bad at his job all things considered. Seems to abuse alcohol, which is a sin.',
 	TheMagicFluteFirstQuest:
 		'Get another guest to play the piano in a duet with you as the vocalist. If nobody wants to play the piano, you may play it yourself and have them sing.',
 	TheMagicFluteSecondQuest:
@@ -29,6 +37,8 @@ export const secrets: Record<string, string> = {
 		'Find some information about each of the guests and spin up a completely far-fetched and inflamatory piece about four of them. The piece must be loosely based on facts.',
 	ThePressIsFreeButLivingIsNotSecondQuest:
 		'Frame a guest for stealing Estelle’s earrings. You are one of the suspects. You win if Richard accuses someone else but you.',
+	ThePressIsFreeButLivingIsNotDickInfo:
+		'You know Dick. Drunken master detective. Somehow solved some unsolveable cases so you have been eyeing him for some time to get a scoop. Sadly it seemed that since the last Christmas party he started drinking even more, and his case solving abilities diminished. No longer a scoop.',
 	AllEyesOnMeFirstQuest:
 		'Pretend you are actually a sister of the hosts (half sister is fine). The hosts will not corroborate or go against your story. Fool half of the guests or more.',
 	AllEyesOnMeSecondQuest: "Whenever something 'outrageous' happens, fake faint. If somebody catches you, thank them with an autograph.",
