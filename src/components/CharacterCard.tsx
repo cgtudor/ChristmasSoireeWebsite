06080c4a5199a991f50d371362d5a26252a20728
cgtudor <tudor.cristian.gabriel@gmail.com>
2023-11-20 16:19:15 +0000

    Moce paper into CharacterCard component

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 import { CharacterPortrait } from '~src/components/CharacterPortrait';
@@ -12,17 +13,19 @@ interface CharacterCardProps {
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ name, description }) => {
 	return (
-		<Grid container spacing={2} alignItems="flex-start">
-			<CharacterPortrait character={name} />
-			<Grid item xs={12} sm={8}>
-				<Box p={2}>
-					<Title text={name} />
-					<Typography variant="body2" color={'#f5f5f5'} sx={{ lineHeight: 1.5, fontFamily: 'Open Sans, sans-serif' }}>
-						{description}
-					</Typography>
-				</Box>
+		<Paper elevation={10} sx={{ p: 2, backgroundColor: '#474747', height: '450px', overflow: 'auto' }}>
+			<Grid container spacing={2} alignItems="flex-start">
+				<CharacterPortrait character={name} />
+				<Grid item xs={12} sm={8}>
+					<Box p={2}>
+						<Title text={name} />
+						<Typography variant="body2" color={'#f5f5f5'} sx={{ lineHeight: 1.5, fontFamily: 'Open Sans, sans-serif' }}>
+							{description}
+						</Typography>
+					</Box>
+				</Grid>
 			</Grid>
-		</Grid>
+		</Paper>
 	);
 };
 
--- src/pages/Guests/Guests.tsx
+++ src/pages/Guests/Guests.tsx
@@ -1,5 +1,4 @@
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 
 import CharacterCard from '~src/components/CharacterCard';
 
@@ -12,9 +11,7 @@ const Guests = () => {
 				{guestsList.map((guest, index) => {
 					return (
 						<Grid item xs={12} sm={6} key={index}>
-							<Paper elevation={10} sx={{ p: 2, backgroundColor: '#474747', height: '450px', overflow: 'auto' }}>
-								<CharacterCard name={guest.name} description={guest.description} />
-							</Paper>
+							<CharacterCard name={guest.name} description={guest.description} />
 						</Grid>
 					);
 				})}
