439164c0c847c4c8325b17a7955592a267577ad0
Calea Dora <dora.calea@gmail.com>
2023-11-30 13:04:44 +0200

    Changed newspaper and home descriptions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,26 +19,31 @@ const imageData = [
 	{ src: MilkGlassImg, alt: 'milk', className: 'milk l-[50%] w-[3em]' },
 	{ src: CookiesImg, alt: 'cookies', className: 'cookies l-[55%] w-[3em]' },
 	{ src: LetterImg, alt: 'letter front', className: 'letter b-[3%] r-[5%] w-[700px]' },
-	{ src: NewspaperImg, alt: 'newspaper', className: 'newspaper t-[1%] r-[5%] w-[500px]' },
+	{ src: NewspaperImg, alt: 'newspaper', className: 'newspaper t-[10%] r-[5%] w-[500px]' },
 	{ src: FrameImg, alt: 'frame', className: 'frame w-[500px]' },
 ];
 
 const cards = [
 	{
 		title: 'Gourmet Delights',
-		description:
-			'We will be serving a variety of food, including but not limited to: appetizers, desserts, alcoholic and non-alcoholic beverages',
+		description: `
+			Ensnare your taste buds with an exquisite selection of rich flavors. We will be serving a variety of food, including but not limited to: appetizers, entrées, christmas cookies, eggnog, hot chocolate and more!
+		`.replace(/\n/g, '<br>'),
 		imageUrl:
 			'https://loveincorporated.blob.core.windows.net/contentimages/gallery/654944e2-c14e-4611-8ad7-ba65e69a9e4b-christmas-family-gathering-1919.jpg',
 	},
 	{
 		title: 'Elegant Atmosphere',
-		description: 'The dress code is formal, so please dress accordingly. We will be taking pictures at the end of the night!',
+		description: `
+			The dress code is formal, so please dress accordingly. We will be taking pictures at the end of the night! Dress to impress and let the spotlight find you at our extraordinary event! Who knows, maybe you will be featured in the next newspaper.
+		`.replace(/\n/g, '<br>'),
 		imageUrl: 'https://www.udiscovermusic.com/wp-content/uploads/2022/02/King-Creole-GettyImages-991925076.jpg',
 	},
 	{
 		title: 'Festive Activities',
-		description: 'Plenty of activities to enjoy during the night: Secret Santa, Family Feud, games and, of course, █████',
+		description: `
+		Unwrap the magic of the season and create unforgettable memories at our Christmas extravaganza! We have prepared plenty of activities to enjoy during the night: Secret Santa, Family Feud, music, games and, of course, ███████!
+		`.replace(/\n/g, '<br>'),
 		imageUrl: 'https://i.pinimg.com/736x/d2/87/6e/d2876eaa2d0a4815d3840b63698d784c.jpg',
 	},
 ];
@@ -59,7 +64,7 @@ const Home = () => {
 			<Grid className="party-cards" container spacing={3} style={{ marginTop: isSmallScreen ? '45%' : '15%' }}>
 				{cards.map((card, index) => (
 					<Grid item key={index} xs={12} sm={6} md={4}>
-						<Card style={{ border: '1px solid #cb9b51', backgroundColor: '#474747', height: 300 }}>
+						<Card style={{ border: '1px solid #cb9b51', backgroundColor: '#474747', height: 350 }}>
 							<CardMedia component="img" height="140" image={card.imageUrl} alt={card.title} />
 							<CardContent>
 								<Typography
@@ -72,7 +77,7 @@ const Home = () => {
 									{card.title}
 								</Typography>
 								<Typography variant="body2" color="silver">
-									{card.description}
+									<div dangerouslySetInnerHTML={{ __html: card.description }} />
 								</Typography>
 							</CardContent>
 						</Card>
