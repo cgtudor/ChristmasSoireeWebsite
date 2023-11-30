6671592311ca88ca92baae674f74011b0c5ff8ad
Calea Dora <dora.calea@gmail.com>
2023-11-30 21:52:37 +0200

    Fix 'de la Croix' and Viktor's name

--- src/pages/Guests/GuestsList.ts
+++ src/pages/Guests/GuestsList.ts
@@ -19,12 +19,12 @@ export const guestsList: GuestsList = [
 	{
 		name: 'Arthur McDonald',
 		description:
-			"Arthur McDonald is a man of enigmatic allure, navigating the bustling cityscape with a mysterious air that left both whispers of admiration and speculation in his wake. What he did, who he was or why he's doing it is of little importance, but he was employed by the Croix family before. Whether perched on a barstool, sipping on a clandestine concoction or leaning against the brick walls of an alleyway, Arthur exuded a charisma that drew people into his orbit.",
+			"Arthur McDonald is a man of enigmatic allure, navigating the bustling cityscape with a mysterious air that left both whispers of admiration and speculation in his wake. What he did, who he was or why he's doing it is of little importance, but he was employed by the 'de la Croix' family before. Whether perched on a barstool, sipping on a clandestine concoction or leaning against the brick walls of an alleyway, Arthur exuded a charisma that drew people into his orbit.",
 	},
 	{
 		name: 'Father Raphael',
 		description:
-			"Father Raphael is a priest from the Vatican. He's a close friend of the Croix family and has personally baptised both of the siblings. While generally not of great renown, he is quite loved by the people that know him, although his preaching can grow tiring at times. In an era marked by societal shifts, Father Raphael stood as a steadfast beacon of spiritual guidance. The hosts believed that Father Raphael's presence would add an air of spirituality to their event, so they once again invited him this year to participate in their festivities.",
+			"Father Raphael is a priest from the Vatican. He's a close friend of the 'de la Croix' family and has personally baptised both of the siblings. While generally not of great renown, he is quite loved by the people that know him, although his preaching can grow tiring at times. In an era marked by societal shifts, Father Raphael stood as a steadfast beacon of spiritual guidance. The hosts believed that Father Raphael's presence would add an air of spirituality to their event, so they once again invited him this year to participate in their festivities.",
 	},
 	{
 		name: 'Genevieve Auclair',
@@ -34,7 +34,7 @@ export const guestsList: GuestsList = [
 	{
 		name: 'Heidi Rosengart',
 		description:
-			"Heidi Rosengart is a musician from Germany. She is a renowned violinist who has performed in many countries around the world. She's caught the favour of the Croix family who has come to love her performances. Whilst she'd generally be invited to perform, this time around she was invited as a guest.",
+			"Heidi Rosengart is a musician from Germany. She is a renowned violinist who has performed in many countries around the world. She's caught the favour of the 'de la Croix' family who has come to love her performances. Whilst she'd generally be invited to perform, this time around she was invited as a guest.",
 	},
 	{
 		name: "Richard O'Connell",
@@ -42,13 +42,13 @@ export const guestsList: GuestsList = [
 			"Richard O'Connell is a tall man with an unkempt beard and an eerie appearance about him. He seems like a true oddball, even in this crowd of eccentrics.",
 	},
 	{
-		name: 'Viktor Koslov',
+		name: 'Viktor Kozlov',
 		description:
-			'Viktor Kozlov is a physicist from the great mother nation. Having published many revolutionary works in the world of physics, he is widely renowned for his logical mind and his ability to solve any problem. He came to know the Croix family through a project he worked on with their father and has been a close friend of the family ever since.',
+			"Viktor Kozlov is a physicist from the great mother nation. Having published many revolutionary works in the world of physics, he is widely renowned for his logical mind and his ability to solve any problem. He came to know the 'de la Croix' family through a project he worked on with their father and has been a close friend of the family ever since.",
 	},
 	{
 		name: 'Finn Fitzgerald',
 		description:
-			'Finn Fitzgerald is a journalist from Ireland. While his relentless pursuit of hard-hitting stories often lands him in trouble, his undeniable charisma gets him out of it at the earliest convenience. With a knack for uncovering scandals and exposing drama, Finn aspires to establish himself as a proeminent figure in the field of investigative journalism. It is of no wonder that when he was invited to a party of the de la Croix family, he was quick to accept. After all, what better place to find a story than a Christmas party thrown by rich socialites?',
+			"Finn Fitzgerald is a journalist from Ireland. While his relentless pursuit of hard-hitting stories often lands him in trouble, his undeniable charisma gets him out of it at the earliest convenience. With a knack for uncovering scandals and exposing drama, Finn aspires to establish himself as a proeminent figure in the field of investigative journalism. It is of no wonder that when he was invited to a party of the 'de la Croix' family, he was quick to accept. After all, what better place to find a story than a Christmas party thrown by rich socialites?",
 	},
 ];
